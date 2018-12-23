f8a0cfb78b1777e074e3b2a6c72414567f6c489c
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-12-23 16:55:01 +0500

    removed commented code

--- src/pages/CoverLetter/NewCoverLetter.js
+++ src/pages/CoverLetter/NewCoverLetter.js
@@ -53,33 +53,3 @@ class NewCoveLetter extends Component {
 }
 
 export default graphql(CreateCoverLetter)(NewCoveLetter);
-
-// import { graphql } from 'react-apollo';
-// import gql from 'graphql-tag';
-//
-// class NewEntry extends Component {
-//   onClick() {
-//     this.props.mutate({
-//       variables: { repoFullName: 'apollographql/apollo-client' }
-//     })
-//       .then(({ data }) => {
-//         console.log('got data', data);
-//       }).catch((error) => {
-//         console.log('there was an error sending the query', error);
-//       });
-//   }
-//
-//   render() {
-//     return <div onClick={this.onClick.bind(this)}>Click me</div>;
-//   }
-// }
-//
-// const submitRepository = gql`
-//   mutation SubmitRepository($repoFullName: String!) {
-//     submitRepository(repoFullName: $repoFullName) {
-//       createdAt
-//     }
-//   }
-// `;
-//
-// const NewEntryWithData = graphql(submitRepository)(NewEntry);

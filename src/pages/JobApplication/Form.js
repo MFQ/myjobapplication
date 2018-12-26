abcd2332e3e142c41c8a11f403af4fc0c867db62
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-12-26 21:21:43 +0500

    refactoed text to textarea

--- src/pages/JobApplication/Form.js
+++ src/pages/JobApplication/Form.js
@@ -27,13 +27,13 @@ const JobApplicationForm = ({
         <Field type="text" name="company" />
         <ErrorMessage name="company" component="div" />
 
-        <Field type="text" name="jobDescription" />
+        <Field type="textarea" name="jobDescription" />
         <ErrorMessage name="jobDescription" component="div" />
 
         <Field type="text" name="status" />
         <ErrorMessage name="status" component="div" />
 
-        <Field type="text" name="coverLetter" />
+        <Field type="textarea" name="coverLetter" />
         <ErrorMessage name="coverLetter" component="div" />
 
         <Field type="text" name="appliedDate" />
@@ -42,7 +42,7 @@ const JobApplicationForm = ({
         <Field type="text" name="source" />
         <ErrorMessage name="source" component="div" />
 
-        <Field type="text" name="response" />
+        <Field type="textarea" name="response" />
         <ErrorMessage name="response" component="div" />
 
         <Field type="text" name="timeTookToApply" />
@@ -51,7 +51,7 @@ const JobApplicationForm = ({
         <Field type="text" name="country" />
         <ErrorMessage name="country" component="div" />
 
-        <Field type="text" name="notes" />
+        <Field type="textarea" name="notes" />
         <ErrorMessage name="notes" component="div" />
 
         <button type="submit" disabled={isSubmitting}>

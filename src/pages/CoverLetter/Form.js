18df2bc6e3f7e4a8e56036a4e9bd8e8214ca0760
Muhammad Fazil Qureshi <softmfq@gmail.com>
2019-01-03 18:17:18 +0500

    added rich textarea to coverletter

--- src/pages/CoverLetter/Form.js
+++ src/pages/CoverLetter/Form.js
@@ -1,7 +1,9 @@
 // @flow
 
-import React from "react";
+import React, { Component } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Editor, EditorState } from "draft-js";
+import { stateToHTML } from "draft-js-export-html";
 
 type Props = {
   onValidate: func,
@@ -10,36 +12,71 @@ type Props = {
   submitLabel: string
 };
 
-const CoverLetterForm = ({
-  initialValues,
-  onValidate,
-  onSubmit,
-  submitLabel
-}: Props) => (
-  <Formik
-    initialValues={initialValues}
-    validate={onValidate}
-    onSubmit={onSubmit}
-  >
-    {({ isSubmitting }) => (
-      <Form>
-        <Field type="text" placeholder="Kind" label="Kind" name="kind" />
-        <ErrorMessage name="kind" component="div" />
+class CoverLetterForm extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = { editorState: EditorState.createEmpty() };
+    // this.onChange = editorState => {
+    //   console.log(editorState.getCurrentContent());
+    //   debugger;
+    //   return this.setState({ editorState });
+    // };
+    this.setEditor = editor => {
+      this.editor = editor;
+    };
+    this.focusEditor = () => {
+      if (this.editor) {
+        this.editor.focus();
+      }
+    };
+  }
 
-        <Field
-          type="text"
-          placeholder="Content"
-          label="content"
-          name="content"
-        />
-        <ErrorMessage name="content" component="div" />
+  componentDidMount() {
+    this.focusEditor();
+  }
 
-        <button type="submit" disabled={isSubmitting}>
-          {submitLabel}
-        </button>
-      </Form>
-    )}
-  </Formik>
-);
+  render() {
+    const { initialValues, onValidate, onSubmit, submitLabel } = this.props;
+    const setState = this.setState.bind(this);
+    return (
+      <Formik
+        initialValues={initialValues}
+        validate={onValidate}
+        onSubmit={onSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="text" placeholder="Kind" label="Kind" name="kind" />
+            <ErrorMessage name="kind" component="div" />
+
+            <Field
+              name="content"
+              render={({ field, form: { isSubmitting, setFieldValue } }) => (
+                <Editor
+                  disabled={isSubmitting}
+                  ref={this.setEditor}
+                  editorState={this.state.editorState}
+                  onChange={editorState => {
+                    console.log(stateToHTML(editorState.getCurrentContent()));
+                    setFieldValue(
+                      "content",
+                      editorState.getCurrentContent().getPlainText()
+                    );
+                    return setState({ editorState });
+                  }}
+                />
+              )}
+            />
+            <ErrorMessage name="content" component="div" />
+
+            <button type="submit" disabled={isSubmitting}>
+              {submitLabel}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    );
+  }
+}
 
 export default CoverLetterForm;
